00543e8c25afdc23d985f82a9151ed3a1869157f
Kuznetsov Aleksey (kalexey89) <kyznecov.alexey@gmail.com>
2017-06-14 15:39:29 +0300

    Added Kill constants for Linux version.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
-module.exports = exports = require('bindings')('ps-native');
+const os = require('os');
+let ps = require('bindings')('ps-native');
+
+if ('linux' === os.platform()) {
+    ps.SIGHUP       = -1;
+    ps.SIGINT       = -2;
+    ps.SIGQUIT      = -3;
+    ps.SIGKILL      = -9;
+    ps.SIGUSR1      = -10;
+    ps.SIGUSR2      = -12;
+    ps.SIGTERM      = -15;
+}
+
+module.exports = exports = ps;
